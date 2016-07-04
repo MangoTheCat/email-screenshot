8192c47bc1aa8288e658b806902107b68f566d2c
paulin.shek@gmail.com <paulin.shek@gmail.com>
2016-07-04 14:18:06 +0100

    correct url

--- capture-screenshot.js
+++ capture-screenshot.js
@@ -1,5 +1,5 @@
 var page = require('webpage').create();
-page.open('http://127.0.0.1:8080/shared.R/rcloud.rcap/rcap.html?notebook=5ddb400c76148387d79c2b5328d39a47', function(){
+page.open('http://www.google.com', function(){
 	page.render('screenshot.png');
 	phantom.exit();
 })
