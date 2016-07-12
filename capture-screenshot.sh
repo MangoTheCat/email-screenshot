ccafd7fa5136715e128a349ae01d36c9b0bf4da8
paulin.shek@gmail.com <paulin.shek@gmail.com>
2016-07-12 10:23:46 +0100

    Get rid of windows path

--- capture-screenshot.sh
+++ capture-screenshot.sh
@@ -1,4 +1,5 @@
+#!/bin/bash
 TMPFILE=$mktemp
 echo $TMPFILE
-"C:\Users\pshek\Documents\phantomjs-2.1.1-windows\bin\phantomjs.exe" --ignore-ssl-errors=true --ssl-protocol=any --debug=true capture-screenshot.js $1 $TMPFILE
+phantomjs --ignore-ssl-errors=true --ssl-protocol=any --debug=true capture-screenshot.js $1 $TMPFILE
 ##echo "A screenshot of RCAP dashboard, sent automatically" | mail -s "RCAP dashboard update" -A $TMPFILE $2
